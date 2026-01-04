21da5ecaf58155293b8b294412e68b89cbf96957
sevdari <88673859+sevdari@users.noreply.github.com>
2026-01-04 15:57:24 +0100

    update: greyjoy

--- week02/greyjoy.cpp
+++ week02/greyjoy.cpp
@@ -1,70 +1,90 @@
+///1
+#include <limits>
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
 #include <algorithm>
 #include <vector>
-#include <cstring>
 
 using namespace std;
 
-const int maxn = 5e4 + 5, maxval = 1e6 + 5;
+long n, k, w; 
 
-typedef long long ll;
-
-int c[maxn];
-int dp[maxval];
-int a[maxn];
-
-void solve() {
-  int n, k, w;
-  cin >> n >> k >> w;
-  for(int i = 0; i < n; i++) {
-    cin >> c[i];
+long window_scan(const vector<long>& waterway){
+  long left = 0, right = 0, current = 0, best = 0;
+  while(right < waterway.size()){
+    current += waterway[right];
+    if(current == k){
+      best = max(best, right - left + 1);
+      current -= waterway[left]; 
+      left++; right++;
+    } else if (current < k){
+      right ++;
+    } else {
+      while(current > k){
+        current -= waterway[left];
+        left ++;
+      }
+      current -= waterway[right];
+    }
   }
-  int sol = 0;
-  ll sum;
-  int l;
-  for(int i = 0; i < w; i++) {
-    cin >> l;
-    sum = 0;
-    for(int j = 0; j < l; j++) {
-      cin >> a[j];
-      sum += c[a[j]];
-      if(sum <= k && dp[k - sum] != -1) {
-        sol = max(sol, dp[k - sum] + j + 1);
-      }
-    }
-    sum = 0;
-    for(int j = 1; j < l; j++) {
-      sum += c[a[j]];
-      if(sum < k) {
-        dp[sum] = max(dp[sum], j);
-      }
-    }
-    int x = 0;
-    sum = 0;
-    for(int j = 0; j < l; j++) {
-      sum += c[a[j]];
-      while(sum > k) {
-        sum -= c[a[x]];
-        x++;
-      }
-      if(sum == k) {
-        sol = max(sol, j - x + 1);
-      }
+  return best;
+}
+
+void testcase() {
+  cin>>n>>k>>w;
+  
+  vector<long> islands(n, 0);
+  vector<vector<long>> waterways(w, vector<long>());
+  for(long i = 0; i < n; i++)
+    cin>>islands[i];
+  
+  for(long i = 0; i < w; i++){
+    long l, temp; cin>>l;
+    for(long j = 0; j < l; j++){
+      cin>>temp; waterways[i].push_back(islands[temp]);
     }
   }
   
-  cout << sol << '\n';
+  long best = 0;
+  
+  // window scans
+  for(long i = 0; i < w; i++)
+    best = max(best, window_scan(waterways[i]));
+    
+  // combining waterways
+  vector<long> memo(k, 0);
+  for(long i = 0; i < w; i++){
+    long current = 0, j = 0;
+    
+    // check for valid combinations
+    while(j < waterways[i].size() && current < k){
+      current += waterways[i][j];
+      if(k - current > 0 && memo[k-current] != 0)
+        best = max(best, j + 1 + memo[k-current]);
+      j++;
+    }
+    
+    // update history
+    current = 0; j = 1;
+    while(j < waterways[i].size() && current < k){
+      current += waterways[i][j];
+      if(current < k)
+        memo[current] = max(memo[current], j);
+      j++;
+    }
+    
+  }
+  
+  cout << best << endl;
 }
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
+  std::ios_base::sync_with_stdio(false);
+
   int t;
-  cin >> t;
-  while(t--) {
-    memset(dp, -1, sizeof(dp));
-    dp[0] = 0;
-    solve();
-  }
-  return 0;
+  std::cin >> t;
+  for (int i = 0; i < t; ++i)
+    testcase();
 }
