0740919b946859fe32ad079b0d2a81bf75aeca11
sevdari <88673859+sevdari@users.noreply.github.com>
2026-01-06 19:34:14 +0100

    update: hit

--- week04/hit.cpp
+++ week04/hit.cpp
@@ -1,36 +1,49 @@
+#include <limits>
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Segment_2.h>
+#include <CGAL/Point_2.h>
+#include <CGAL/Ray_2.h>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef K::Point_2 P;
-typedef K::Ray_2 R;
-typedef K::Segment_2 S;
 
-void testcase(int n){
+typedef CGAL::Segment_2< K > Segment;
+typedef CGAL::Point_2< K >  Point;
+typedef CGAL::Ray_2< K > Ray;
+
+void testcase(int n) {
   long x, y, a, b; cin>>x>>y>>a>>b;
-  R ray = R(P(x, y), P(a, b));
-  
+  Ray photon = Ray(Point(x, y), Point(a, b));
+  vector<Segment> walls;
   for(int i = 0; i < n; i++){
     long r, s, t, u; cin>>r>>s>>t>>u;
-    S current = S(P(r, s), P(t, u));
-    
-    if(CGAL::do_intersect(ray, current)){
-      for (; i < n - 1; i++)
-        cin >> r >> s >> t >> u;
-      cout << "yes" << endl;
-      return;
-    }
+    Segment wall = Segment(Point(r, s), Point(t, u));
+    walls.push_back(wall);
   }
-  cout << "no" << endl;
+  
+  for(auto wall: walls){
+    if (CGAL::do_intersect(photon, wall)){
+      cout << "yes\n"; return;
+    } 
+  }
+  cout << "no\n";
+  
 }
 
-int main(){
-  ios_base::sync_with_stdio(false);
-  int n; cin>>n;
+int main() {
+  std::ios_base::sync_with_stdio(false);
+
+  int n;
+  std::cin >> n;
   while(n!=0){
-    testcase(n);
-    cin>>n;
+    testcase(n); cin>>n;
   }
 }
