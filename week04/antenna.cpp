f791336549811391505f21299ce7b083a49a6821
sevdari <88673859+sevdari@users.noreply.github.com>
2026-01-06 21:21:19 +0100

    update: antenna

--- week04/antenna.cpp
+++ week04/antenna.cpp
@@ -1,14 +1,19 @@
+#include <limits>
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <algorithm>
 #include <vector>
+
 using namespace std;
 
 #include <CGAL/Exact_predicates_exact_constructions_kernel_with_sqrt.h>
-
 #include <CGAL/Min_circle_2.h>
 #include <CGAL/Min_circle_2_traits_2.h>
 
 typedef CGAL::Exact_predicates_exact_constructions_kernel_with_sqrt K;
-typedef K::Point_2 P;
+
 typedef CGAL::Min_circle_2_traits_2<K> Traits;
 typedef CGAL::Min_circle_2<Traits> Min_circle;
 
@@ -19,25 +24,25 @@ double ceil_to_double(const K::FT& x) {
   return a;
 }
 
-
-void testcase(int n){
-  std::vector<K::Point_2> houses;
-  
+void testcase(int n) {
+  vector<K::Point_2> citizens;
   for(int i = 0; i < n; i++){
-    double x, y; cin>>x>>y;
-    houses.push_back(P(x, y));
+    long x, y; cin>>x>>y;
+    citizens.push_back(K::Point_2(x, y));
   }
-  Min_circle mc(houses.begin(), houses.end(), true);
+  
+  Min_circle mc(citizens.begin(), citizens.end(), true);
   Traits::Circle c = mc.circle();
-  cout << ceil_to_double(CGAL::sqrt(c.squared_radius())) << endl;
+  K::FT d = CGAL::sqrt(c.squared_radius());
+  std::cout << ceil_to_double(d) << "\n";
 }
 
-int main(){
-  ios_base::sync_with_stdio(false);
+int main() {
+  std::ios_base::sync_with_stdio(false);
   std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(0);
-  int n; cin>>n;
+
+  int n; cin >> n;
   while(n!=0){
-    testcase(n);
-    cin>>n;
+    testcase(n); cin>>n;
   }
 }
