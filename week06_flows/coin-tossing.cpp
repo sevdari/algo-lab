de35e5d53ea7a372c7798e84223d69fd5eadd02f
sevdari <88673859+sevdari@users.noreply.github.com>
2026-01-10 21:53:50 +0100

    update: coin tossing

--- week06_flows/coin-tossing.cpp
+++ week06_flows/coin-tossing.cpp
@@ -1,90 +1,101 @@
+///1
+#include <limits>
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <algorithm>
 #include <vector>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/push_relabel_max_flow.hpp>
+
 using namespace std;
 
-// BGL graph type
-typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> traits;
-typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
-    boost::no_property,
-    boost::property<boost::edge_capacity_t, long,
-    boost::property<boost::edge_residual_capacity_t, long,
-    boost::property<boost::edge_reverse_t, traits::edge_descriptor>>>> graph;
+// BGL include
+#include <boost/graph/adjacency_list.hpp>
 
+// BGL flow include *NEW*
+#include <boost/graph/push_relabel_max_flow.hpp>
+
+// Graph Type with nested interior edge properties for flow algorithms
+typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> traits;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
+    boost::property<boost::edge_capacity_t, long,
+        boost::property<boost::edge_residual_capacity_t, long,
+            boost::property<boost::edge_reverse_t, traits::edge_descriptor>>>> graph;
+
+typedef traits::vertex_descriptor vertex_desc;
 typedef traits::edge_descriptor edge_desc;
 
-struct edge_adder {
-    graph &G;
-    explicit edge_adder(graph &G) : G(G) {}
+class edge_adder {
+  graph &G;
 
-    void add_edge(int u, int v, long c) {
-        auto cap = boost::get(boost::edge_capacity, G);
-        auto rev = boost::get(boost::edge_reverse, G);
-        const auto e = boost::add_edge(u, v, G).first;
-        const auto r = boost::add_edge(v, u, G).first;
-        cap[e] = c;
-        cap[r] = 0;
-        rev[e] = r;
-        rev[r] = e;
-    }
+ public:
+  explicit edge_adder(graph &G) : G(G) {}
+
+  void add_edge(int from, int to, long capacity) {
+    auto c_map = boost::get(boost::edge_capacity, G);
+    auto r_map = boost::get(boost::edge_reverse, G);
+    const auto e = boost::add_edge(from, to, G).first;
+    const auto rev_e = boost::add_edge(to, from, G).first;
+    c_map[e] = capacity;
+    c_map[rev_e] = 0; // reverse edge has no capacity!
+    r_map[e] = rev_e;
+    r_map[rev_e] = e;
+  }
 };
 
-void testcase() {
-  
-  long players, rounds; cin>>players>>rounds;
-  vector<long> points(players, 0);
-  vector<long> finish(players, 0);
-  vector<pair<long, long>> games;
-  long points_so_far = 0, total_points = 0;
-  for(long i = 0; i < rounds; i++){
-    long a, b, c; cin>>a>>b>>c;
-    if(c==1){
-      points[a]++;
-      points_so_far++;
-    } else if(c==2){
-      points[b]++;
-      points_so_far++;
-    } else {
-      games.push_back({a, b});
-    }
-  }
-  
-  long games_remaining = games.size();
-  graph G(games_remaining + players + 2);
-  edge_adder adder(G);
-  long s = games_remaining + players, t = games_remaining + players + 1;
-  
-  bool valid = true;
-  for(long i = 0; i < players; i++){
-    long score; cin>>score;
-    finish[i] = score;
-    if(score < points[i]) valid = false;
-    total_points += score;
-    adder.add_edge(games_remaining + i, t, score - points[i]);
-  }
-  
-  if(!valid || total_points - points_so_far != games.size()){
-    cout << "no\n";
-    return;
-  }
 
-  for(long i = 0; i < games_remaining; i++){
-    adder.add_edge(s, i, 1);
-    auto [a, b] = games[i];
-    adder.add_edge(i, games_remaining + a, 1);
-    adder.add_edge(i, games_remaining + b, 1);
+void testcase() {
+  int n, m; cin>>n>>m;
+  vector<int> points(n, 0);
+  vector<pair<int, int>> matches;
+  
+  int a, b, c;
+  for(int i = 0; i < m; i++){
+    cin>>a>>b>>c;
+    if(c == 0)
+      matches.push_back({a, b});
+    else
+      if(c==1) points[a]++; else points[b]++;
   }
   
-  long flow = boost::push_relabel_max_flow(G, s, t);
-  cout << (flow == total_points - points_so_far ? "yes" : "no") << "\n";
+  int s;
+  bool flag = false;
+  int need = 0;
+  for(int i = 0; i < n; i++){
+    cin>>s;
+    if(s < points[i])
+      flag = true;
+    points[i] = s - points[i];
+    need += points[i];
+  }
+  
+  if(flag || need!=matches.size()){ cout << "no\n"; return;}
+  
+  graph G(matches.size() + n);
+  edge_adder adder(G);
+  
+  const vertex_desc v_source = boost::add_vertex(G);
+  const vertex_desc v_sink = boost::add_vertex(G);
+  
+  for(size_t i = 0; i < matches.size(); i++){
+    auto [a, b] = matches[i];
+    adder.add_edge(v_source, i, 1);
+    adder.add_edge(i, matches.size() + a, 1);
+    adder.add_edge(i, matches.size() + b, 1);
+  }
+  
+  for(size_t i = 0; i < n; i++)
+    adder.add_edge(matches.size() + i, v_sink, points[i]);
+  
+  long flow = boost::push_relabel_max_flow(G, v_source, v_sink);
+  cout << (flow == need ? "yes\n": "no\n");
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
 
-    int t;
-    cin >> t;
-    while (t--) testcase();
+  int t;
+  std::cin >> t;
+  for (int i = 0; i < t; ++i)
+    testcase();
 }
