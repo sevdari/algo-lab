a0fa0cf356abec3fc139df44b31f0feaa30bfcd3
sevdari <88673859+sevdari@users.noreply.github.com>
2026-02-01 20:49:07 +0100

    update: legions

--- week11/legions.cpp
+++ week11/legions.cpp
@@ -1,13 +1,4 @@
-///2
-
-// Solves just test case 2, gotta take care of the sign for the others/
-#include <limits>
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <cmath>
-#include <algorithm>
-#include <vector>
+#include<bits/stdc++.h>
 
 using namespace std;
 
@@ -15,6 +6,7 @@ using namespace std;
 #include <CGAL/QP_functions.h>
 #include <CGAL/Gmpz.h>
 
+// choose input type (input coefficients must fit)
 typedef int IT;
 // choose exact type for solver (CGAL::Gmpz or CGAL::Gmpq)
 typedef CGAL::Gmpz ET;
@@ -24,52 +16,59 @@ typedef CGAL::Quadratic_program<IT> Program;
 typedef CGAL::Quadratic_program_solution<ET> Solution;
 
 struct Line{
-  int a;
-  int b;
-  int c;
-  int speed;
-  int normal;
+  long a;
+  long b;
+  long c;
+  long v;
+  long norm;
 };
 
-void testcase() {
-  int x, y, n; cin>>x>>y>>n;
-  vector<Line> lines(n);
-  for(int i = 0; i < n; i++){
-    cin>>lines[i].a>>lines[i].b>>lines[i].c>>lines[i].speed;
-    // TODO: Check if we need exact sqrt
-    lines[i].normal = CGAL::to_double(CGAL::sqrt(1.0 * lines[i].a * lines[i].a + 1.0 * lines[i].b * lines[i].b)); 
-  }
-  
-  Program lp (CGAL::SMALLER, false, 0, false, 0); 
-  const int t = 0;
-  const int p_x = 1;
-  const int p_y = 2;
-  
-  lp.set_l(t, 0);
-  lp.set_c(t, -1); 
-  
-  // legions constraint
-  for(int i = 0; i < n; i++){
-    // t_i * v_i * norm_i
-    lp.set_a(t, i,  lines[i].speed * lines[i].normal); 
-    // + a_i * p_x + b_i * p_y
-    lp.set_a(p_x, i, lines[i].a); lp.set_a(p_y, i, lines[i].b); 
-    // <= -c
-    lp.set_b(i, -lines[i].c);
-  }
-  
-  Solution s = CGAL::solve_linear_program(lp, ET());
-  cout << floor(CGAL::to_double(-s.objective_value().numerator() / 
-          s.objective_value().denominator()))
-       << endl;
+double floor_to_double(const CGAL::Quotient<ET>& x)
+{
+  double a = std::floor(CGAL::to_double(x));
+  while (a > x) a -= 1;
+  while (a+1 <= x) a += 1;
+  return a;
 }
 
-int main() {
-  std::ios_base::sync_with_stdio(false);
-  std::cout << std::fixed << std::setprecision(0);
+void testcase(){
+  int x, y, n; cin>>x>>y>>n;
+  vector<Line> lines(n);
+  
+  // add lines, ensure correct orientation
+  for(int i = 0; i < n; i++){
+    cin>>lines[i].a>>lines[i].b>>lines[i].c>>lines[i].v;
+    if(lines[i].a * x + lines[i].b * y + lines[i].c < 0){
+      lines[i].a *= -1; lines[i].b *= -1; lines[i].c *= -1;
+    }
+    lines[i].norm = lines[i].a * lines[i].a + lines[i].b * lines[i].b;
+    lines[i].norm = sqrt(lines[i].norm);
+  }
+  
+  Program lp (CGAL::LARGER, false, 0, false, 0); 
+  const int X = 0; 
+  const int Y = 1;
+  const int T = 2;
+  
+  for(int i = 0; i < n; i++){
+    auto line = lines[i];
+    lp.set_a(X, i,  line.a); lp.set_a(Y, i, line.b);
+    lp.set_a(T, i, -line.norm * line.v);
+    lp.set_b(i, -line.c);
+  }
+  lp.set_c(T, -1);
+  
+  lp.set_l(T, true, 0);
+  
+  Solution s = CGAL::solve_linear_program(lp, ET());
+  cout << floor_to_double(-s.objective_value()) << endl;
+  
+}
 
-  int t;
-  std::cin >> t;
-  for (int i = 0; i < t; ++i)
-    testcase();
+int main(){
+  std::ios_base::sync_with_stdio(false);
+  std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(0);
+  std::size_t t;
+  for (std::cin >> t; t > 0; --t) testcase();
+  return 0;
 }
