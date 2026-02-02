c5fdd09224be005c9dd2d2cdff8a16fbffc7978a
sevdari <88673859+sevdari@users.noreply.github.com>
2026-02-02 16:14:40 +0100

    update: important bridges

--- week03_bgl/important_bridges.cpp
+++ week03_bgl/important_bridges.cpp
@@ -1,70 +1,65 @@
-// This is pretty much a copy of the solution provided in the lecture slides.
-
-#include <limits>
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <cmath>
-#include <algorithm>
-#include <vector>
-#include <cassert>
+///1
+#include<bits/stdc++.h>
 
 using namespace std;
 
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/biconnected_components.hpp>
 
-typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,
- boost::no_property, // no vertex properties
- boost::property<boost::edge_name_t, int>> Graph;
-
-typedef boost::property_map<Graph, boost::edge_name_t>::type ComponentMap; 
-
-typedef boost::graph_traits<Graph>::edge_iterator EdgeIt;
-typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
-typedef boost::graph_traits<Graph>::edge_descriptor Edge;
-
-void testcase() {
-  // input
-  int V, E; cin>>V>>E;
-  Graph G(V);
-  for(int i = 0; i < E; i++){
-    int u, v; cin>>u>>v;
-    boost::add_edge(u, v, G);
-  }
-  
-  // solution
-  ComponentMap componentmap = boost::get(boost::edge_name, G);
-  int ncc = boost::biconnected_components(G, componentmap);
-  std::vector<int> componentsize(ncc);
-  EdgeIt ebeg, eend;
-  for (tie(ebeg, eend) = boost::edges(G); ebeg != eend; ++ebeg) {
-    componentsize[componentmap[*ebeg]]++;
-  }
-  // Solution vector to record bridges
-  std::vector<std::pair<int,int> > bridges;
-  for (boost::tie(ebeg, eend) = boost::edges(G); ebeg != eend; ++ebeg) {
-    if (componentsize[componentmap[*ebeg]] == 1) { // If edge in a
-      int u = source(*ebeg, G); // component of size = 1
-      int v = target(*ebeg, G);
-      bridges.push_back(std::make_pair(std::min(u,v),
-      std::max(u,v)));
-    }
-  }
-  std::sort(bridges.begin(), bridges.end());
-  // Output
-  std::cout << bridges.size() << std::endl;
-  for (size_t i = 0; i < bridges.size(); ++i) {
-    std::cout << bridges[i].first << " " <<
-    bridges[i].second << std::endl;
-  }
+namespace boost
+{
+struct edge_component_t
+{
+    enum
+    {
+        num = 555
+    };
+    typedef edge_property_tag kind;
+} edge_component;
 }
 
-int main() {
-  std::ios_base::sync_with_stdio(false);
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,
+          boost::no_property, boost::property< boost::edge_component_t, std::size_t > >          graph;
+typedef boost::graph_traits<graph>::vertex_descriptor                       vertex_desc;
 
-  int t;
-  std::cin >> t;
-  for (int i = 0; i < t; ++i)
-    testcase();
+void testcase(){
+  int n, m; cin>>n>>m;
+  int u, v;
+  graph G(n);
+  for(int i = 0; i < m; i++){
+    cin>>u>>v;
+    boost::add_edge(u, v, G);
+  }
+  boost::property_map< graph, boost::edge_component_t >::type component
+        = get(boost::edge_component, G);
+   std::size_t num_comps = boost::biconnected_components(G, component);
+  
+  vector<int> freq(m, 0);
+  boost::graph_traits< graph >::edge_iterator ei, ei_end;
+  for (boost::tie(ei, ei_end) = boost::edges(G); ei != ei_end; ++ei)
+      freq[component[*ei]] += 1;
+  
+  vector<pair<int, int>> ans;
+  for (boost::tie(ei, ei_end) = boost::edges(G); ei != ei_end; ++ei){
+    if(freq[component[*ei]] == 1){
+      if((int)boost::source(*ei, G) < (int)boost::target(*ei, G)){
+        ans.push_back({boost::source(*ei, G), boost::target(*ei, G)});
+      } else {
+        ans.push_back({boost::target(*ei, G), boost::source(*ei, G)});
+      }
+    }
+  }
+  sort(ans.begin(), ans.end());
+  cout << ans.size() << endl;
+  for(auto [u,v]: ans)
+    cout << u << " " << v << endl;
+  
+}
+
+int main(){
+  std::ios_base::sync_with_stdio(false);
+  std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(0);
+  std::size_t t;
+  for (std::cin >> t; t > 0; --t) testcase();
+  return 0;
 }
